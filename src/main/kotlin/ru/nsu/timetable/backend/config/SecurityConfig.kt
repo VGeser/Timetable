e69d2c552e5f3a54f8346a8cee23f7e1ca552b30
Merzlyakov Ilya <leadpogrommer@yandex-team.ru>
2022-11-25 13:40:10 +0700

    Disable cors

--- src/main/kotlin/ru/nsu/timetable/backend/config/SecurityConfig.kt
+++ src/main/kotlin/ru/nsu/timetable/backend/config/SecurityConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import ru.nsu.timetable.backend.security.jwt.JwtConfigurer
 import ru.nsu.timetable.backend.security.jwt.JwtTokenProvider
 
@@ -26,5 +28,12 @@ class SecurityConfig(private val jwtTokenProvider: JwtTokenProvider): WebSecurit
             .anyRequest().authenticated()
             .and()
             .apply<SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>>(JwtConfigurer(jwtTokenProvider))
+    }
+}
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
     }
 }
