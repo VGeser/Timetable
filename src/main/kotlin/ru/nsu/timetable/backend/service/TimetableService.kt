70747ca1aaa49084bf2a6ca528121a846914be38
leadpogrommer <ilyamerzlakov@gmail.com>
2022-11-28 20:46:37 +0700

    Add generator interface

---
+++ src/main/kotlin/ru/nsu/timetable/backend/service/TimetableService.kt
@@ -0,0 +1,43 @@
+package ru.nsu.timetable.backend.service
+
+import org.springframework.stereotype.Service
+import ru.nsu.timetable.backend.entity.Slot
+import ru.nsu.timetable.backend.entity.TimetableEntry
+import ru.nsu.timetable.backend.repo.*
+
+
+@Service
+class RepoProvider(
+    val slots: SlotRepository,
+    val teachers: TeacherRepository,
+    val courses: CourseRepository,
+    val rooms: RoomRepository,
+    val groups: GroupRepository,
+)
+
+
+abstract class TimetableGeneratorService(
+    private val repos: RepoProvider,
+) {
+    abstract fun generate(): List<TimetableEntry>
+}
+
+
+class TimetableConflict(val priority: Int, val slot: Slot?, val message: String)
+
+abstract class TimetableValidator(
+    private val repos: RepoProvider,
+) {
+    abstract fun validate(): List<TimetableConflict>
+}
+
+
+class TimetableService {
+    fun generateTimetable(){
+
+    }
+
+    fun validateTimetable(){
+
+    }
+}
